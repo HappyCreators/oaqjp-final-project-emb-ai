7363a5163a59088fde4ba74f608701887ab91cb8
HappyCreators <bestself.happysoul@gmail.com>
2025-07-30 18:16:06 -0400

    Formatted emotion_detector function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,7 +7,30 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     
     response = requests.post(url, json = myobj, headers=header)
+    formatted_response = response.json()
 
-    return response.text
+    if response.status_code == 200:
+        emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
+        anger = emotion_dict['anger']
+        disgust = emotion_dict['disgust']
+        fear = emotion_dict['fear']
+        joy = emotion_dict['joy']
+        sadness = emotion_dict['sadness']
+        dominant_emotion = max(emotion_dict, key=emotion_dict.get) 
+    else:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
+    
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
 
 
